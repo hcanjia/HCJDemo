24ada1ac9755a1b21a072aede25918aed7b76ce8
hcjge <1057108851@qq.com>
2017-09-20 17:16:08 +0800

    Update wx.php

--- application/extra/wx.php
+++ application/extra/wx.php
@@ -7,8 +7,8 @@
  */
 
 return [
-    'app_id' => 'wxf84bed23a25ec712',
-    'app_secret' => '6d4a158d4e848f6612de42503e1d81e7',
+    'app_id' => '******',
+    'app_secret' => '*****',
     'login_url' => 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code',
     'access_token' =>'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s',
     'get_wx_code_un_limit'=>'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=%s'
